3166398c44cd44c737fceab8a217afc782c902bf
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-04-18 15:25:36 -0300

    put-messages-on-screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,54 @@ async function login() {
     if (loginStatus !== 200) {
         location.reload();
     }
+    await keepLoginAlive(userName);
 
-    const authMessages = await getMessages(userName.value);
-    console.log(authMessages);
-    await keepLoginAlive(userName.value);
+    const authMessages = await getMessages(userName);
+    await appendMessages(userName, authMessages);
+    await keepMessagesUpdate(userName);
 
     container.classList.remove('login');
     login.style.display = 'none';
     batePapo.style.display = 'block';
 }
 
-async function getMessages(name) {
+async function keepMessagesUpdate(username) {
+    setInterval(async () => {
+        const authMessages = await getMessages(username);
+
+        if (authMessages.length > sessionStorage.getItem('messagesLength')) {
+            await appendMessages(username, authMessages)
+        }
+    }, 1000);
+}
+
+async function appendMessages(username, authMessages) {
+    sessionStorage.setItem('messagesLength', authMessages.length);
+    const messagesEl = document.getElementById('messages');
+
+    authMessages.forEach(message => {
+        if (message) {
+            if (message.type === 'status') {
+                messagesEl.innerHTML += `<div class="${message.type}">
+                                        <span class="time">(${message.time})</span>
+                                        <span class="username"><strong>${message.from}</strong> ${message.text}</span>
+                                    </div>`;
+            } else {
+                messagesEl.innerHTML += `<div class="${message.type}">
+                                        <span class="time">(${message.time})</span>
+                                        <span class="username"><strong>${message.from}</strong> para <strong>${message.to}</strong> ${message.text}</span>
+                                    </div>`;
+            }
+        }
+    })
+
+}
+
+async function getMessages(username) {
     const messages = await messagesRequest();
 
     return messages.data.map(message => {
-        if (message.to === 'Todos' || message.to === name) {
+        if (message.to === 'Todos' || message.to === username.value) {
             return message;
         }
     });
@@ -41,8 +74,8 @@ async function loginRequest(name) {
     return response.status;
 }
 
-async function keepLoginAlive(name) {
-    const intervalId = setInterval(async () => await statusAliveRequest(name), 3000);
+async function keepLoginAlive(username) {
+    setInterval(async () => await statusAliveRequest(username.value), 3000);
 }
 
 async function statusAliveRequest(name) {
